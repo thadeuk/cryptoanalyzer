cfbb7701aa20f8968ba37f7b0b6f1d9375e1c3a2
Thadeu <thadeutucci@gmail.com>
2018-03-21 13:58:55 +0100

    making request with cors

--- src/Crawler.js
+++ src/Crawler.js
@@ -4,26 +4,38 @@ import logo from './logo.svg';
 var request = require('request');
 var cheerio = require('cheerio');
 var URL = require('url-parse');
-
-var pageToVisit = "http://coinmarketcap.com/historical/20171119/";
-console.log("Visiting page " + pageToVisit);
-request(pageToVisit, function(error, response, body) {
-   if(error) {
-     console.log("Error: " + error);
-     return;
-   }
-   // Check status code (200 is HTTP OK)
-   
-    console.log(response);
-   console.log("Status code: " + response.statusCode);
-   if(response.statusCode === 200) {
-     // Parse the document body
-     var $ = cheerio.load(body);
-     console.log("Page title:  " + $('title').text());
-   }
-});
+var pageToVisit = "https://coinmarketcap.com/historical/20171119/";
 
 class Crawler extends Component {
+    constructor() {
+        super();
+        this.state = {
+            message: ''
+        }
+    }
+
+    runCrawler = () => {
+        console.log("Visiting page " + pageToVisit);
+        this.setState({message : 'Crawling... wait a moment...'});
+        var options = {
+            url: "https://cors-anywhere.herokuapp.com/"+ pageToVisit
+        };
+        request(options, function(error, response, body) {
+           if(error) {
+             console.log("Error: " + error);
+             return;
+           }
+           // Check status code (200 is HTTP OK)
+
+           console.log("Status code: " + response.statusCode);
+           if(response.statusCode === 200) {
+             // Parse the document body
+             var $ = cheerio.load(body);
+             console.log("Page title:  " + $('title').text());
+           }
+        });
+  };
+
   render() {
     return (
       <div className="Crawler">
@@ -32,7 +44,8 @@ class Crawler extends Component {
           <h1 className="Crawler-title">Welcome to CryptoCrawler</h1>
         </header>
         <p className="Crawler-intro">
-            Crawling... wait a moment...
+            <button onClick={this.runCrawler}>Load</button>
+            {this.state.message}
         </p>
       </div>
     );
